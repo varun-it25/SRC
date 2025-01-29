4f65d5de97110637bd96bd289dc2a663de281f43
varun kangotra <varunkangotra.it25@gmail.com>
2025-01-29 17:48:32 +0530

    Create-Event Works properly

--- src/pages/Create-Event.tsx
+++ src/pages/Create-Event.tsx
@@ -78,38 +78,18 @@ const CreateEvent = () => {
     }
   }
 
-  const isFormValid =
-    eventName &&
-    venue &&
-    description &&
-    eventDate &&
-    eventStartTime &&
-    eventEndTime &&
-    guestName &&
-    guestEmail &&
-    guestPhone &&
-    banner
-
+  const isFormValid = eventName && venue && description && eventDate && eventStartTime && eventEndTime && guestName && guestEmail && guestPhone && banner
+  
   return (
     <Container>
       <div className="w-full flex-col flex h-fit p-6 space-y-6">
-        <div className="w-full">
-          <p className="font-bold text-2xl">Create New Event</p>
-        </div>
-
+        <div className="w-full"><p className="font-bold text-2xl">Create New Event</p></div>
         {loading ? (
-          <div className="flex justify-center items-center h-full space-x-4">
-            <UploadCloud size={40} className="animate-spin" />
-            <p>Uploading...</p>
-          </div>
+          <div className="flex justify-center items-center h-full space-x-4"><UploadCloud size={40} className="animate-spin" /><p>Uploading...</p></div>
         ) : status === 'success' ? (
-          <div className="w-full bg-green-200 text-center p-4 rounded-lg">
-            <p className="font-semibold text-xl">Event Created Successfully!</p>
-          </div>
+          <div className="w-full bg-green-200 text-center p-4 rounded-lg"><p className="font-semibold text-xl">Event Created Successfully!</p></div>
         ) : status === 'error' ? (
-          <div className="w-full bg-red-200 text-center p-4 rounded-lg">
-            <p className="font-semibold text-xl">{errorMessage}</p>
-          </div>
+          <div className="w-full bg-red-200 text-center p-4 rounded-lg"><p className="font-semibold text-xl">{errorMessage}</p></div>
         ) : (
           <>
             <div className="w-full bg-white border rounded-lg p-4 sm:p-6">
@@ -117,47 +97,33 @@ const CreateEvent = () => {
                 <p className="py-1 bg-blue-200 px-4 font-semibold rounded-full">Step 1</p>
                 <p className="font-semibold text-lg">Event Information</p>
               </div>
-
               <div className="p-1 sm:p-3 w-full space-y-5">
                 {banner ? (
-                  <label
-                    htmlFor="banner"
-                    className="w-full h-28 sm:h-52 rounded-xl flex justify-center text-zinc-600 items-center border-zinc-300 space-x-2 text-xl sm:text-2xl font-bold border-2 border-dashed bg-zinc-100 cursor-pointer hover:bg-zinc-200"
-                  >
+                  <label htmlFor="banner" className="w-full h-28 sm:h-52 rounded-xl flex justify-center text-zinc-600 items-center border-zinc-300 space-x-2 text-xl sm:text-2xl font-bold border-2 border-dashed bg-zinc-100 cursor-pointer hover:bg-zinc-200">
                     <img src={URL.createObjectURL(banner)} className="h-full w-full rounded-xl object-cover" />
                   </label>
                 ) : (
-                  <label
-                    htmlFor="banner"
-                    className="w-full h-28 sm:h-52 rounded-xl flex justify-center text-zinc-600 items-center border-zinc-300 space-x-2 text-xl sm:text-2xl font-bold border-2 border-dashed bg-zinc-100 cursor-pointer hover:bg-zinc-200"
-                  >
-                    <UploadCloud size={30} />
-                    <p>Banner</p>
+                  <label htmlFor="banner" className="w-full h-28 sm:h-52 rounded-xl flex justify-center text-zinc-600 items-center border-zinc-300 space-x-2 text-xl sm:text-2xl font-bold border-2 border-dashed bg-zinc-100 cursor-pointer hover:bg-zinc-200">
+                    <UploadCloud size={30} /><p>Banner</p>
                   </label>
                 )}
-
                 <input id="banner" type="file" className="hidden" onChange={(e) => handleFileChange(e, setBanner)} />
-
                 <div className="space-y-1">
                   <p className="font-semibold text-xs">Name</p>
                   <Input value={eventName} onChange={(e) => setEventName(e.target.value)} placeholder="Jigyasa" />
                 </div>
-
                 <div className="space-y-1">
                   <p className="font-semibold text-xs">Venue</p>
                   <Input value={venue} onChange={(e) => setVenue(e.target.value)} placeholder="D-Block, Auditorium, JECRC" />
                 </div>
-
                 <div className="space-y-1">
                   <p className="font-semibold text-xs">Description</p>
                   <Textarea rows={5} value={description} onChange={(e) => setDescription(e.target.value)} placeholder="Write something here..." />
                 </div>
-
                 <div className="space-y-1">
                   <p className="font-semibold text-xs">Date</p>
                   <Input type="date" value={eventDate} onChange={(e) => setEventDate(e.target.value)} />
                 </div>
-
                 <div className="space-y-1 w-full">
                   <p className="font-semibold text-xs">Time</p>
                   <div className="w-full flex gap-4 items-center">
@@ -168,13 +134,11 @@ const CreateEvent = () => {
                 </div>
               </div>
             </div>
-
             <div className="w-full bg-white border rounded-lg p-6">
               <div className="flex space-x-3 items-center mb-4">
                 <p className="py-1 bg-red-200 px-4 font-semibold rounded-full">Step 2</p>
                 <p className="font-semibold text-lg">Guest Information</p>
               </div>
-
               <div className="p-3 w-full space-y-5">
                 <div className="w-full flex space-x-4 items-center font-semibold text-lg">
                   {guestImage ? (
@@ -186,7 +150,6 @@ const CreateEvent = () => {
                       <User size={40} />
                     </div>
                   )}
-
                   {guestImage ? (
                     <div className="flex space-x-3">
                       <label htmlFor="guestImage" className="hover:underline text-yellow-500 cursor-pointer">Update</label>
@@ -197,27 +160,22 @@ const CreateEvent = () => {
                   )}
                 </div>
                 <input id="guestImage" type="file" className="hidden" onChange={(e) => handleFileChange(e, setGuestImage)} />
-
                 <div className="space-y-1">
                   <p className="font-semibold text-xs">Name</p>
                   <Input value={guestName} onChange={(e) => setGuestName(e.target.value)} placeholder="Sister Shivani" />
                 </div>
-
                 <div className="space-y-1">
                   <p className="font-semibold text-xs">Email ID</p>
                   <Input type="email" value={guestEmail} onChange={(e) => setGuestEmail(e.target.value)} placeholder="shivani@gmail.com" />
                 </div>
-
                 <div className="space-y-1">
                   <p className="font-semibold text-xs">Mobile No.</p>
                   <Input type="tel" value={guestPhone} onChange={(e) => setGuestPhone(e.target.value)} placeholder="+91 98765-43210" />
                 </div>
               </div>
             </div>
-
             <Button className="flex space-x-2" onClick={handleSubmit} disabled={!isFormValid}>
-              <UploadCloud size={30} />
-              <p>Publish</p>
+              <UploadCloud size={30} /><p>Publish</p>
             </Button>
           </>
         )}
